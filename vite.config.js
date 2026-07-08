b79a081c5cf91193f2fbf860f516b59a8d92b89f
yuqingxing22 <kyraxing@gmail.com>
2026-07-08 11:38:36 -0400

    Use root asset paths for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const base = process.env.GITHUB_ACTIONS && repoName ? `/${repoName}/` : "/";
-
 export default defineConfig({
-  base,
+  base: "/",
   plugins: [react()],
 });
